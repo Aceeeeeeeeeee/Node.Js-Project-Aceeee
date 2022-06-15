c04555f23ea9f934c9bad9a06bb6698b6ba0e395
Aceeeeeeeeeee <105403890+Aceeeeeeeeeee@users.noreply.github.com>
2022-06-15 13:58:55 +0700

    new

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const chalk = require('chalk')
-const debug = require('debug')('app')
-const morgan = require('morgan')
-const app = express()
-const port = 9000
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const app = express();
+const port = 9000;
+const path = require('path');
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public")));
+
+
 app.get("/", (req,res) =>
 {
     res.send('Hello Taweechok Ace');
